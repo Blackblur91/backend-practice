e0818faec991f320ef83050e93f9b72e64194e59
Blackblur91 <feketebalazs04@gmail.com>
2023-12-07 09:28:20 +0100

    fixing code ws

--- index.js
+++ index.js
@@ -32,16 +32,49 @@ app.get('/users', (req, res) => {
 })
 
 app.get('/users/:userid', (req, res) => {
-  res.send(req.params.userid)    /* :utáni rész userid néven jön létre a backenden a request params-ban */
-})
+  // res.send(req.params.userid)
+  // const userId = Number(req.params.userid)
 
-fs.readFile( '"text.txt', "utf-8", (err, data) => {  //<-- paraméterek: fájl elérési útja/neve, kódolás, callback függvény, amiben 2 paramétert várunk: error object
-  if( err) {
+  /* a bejövő paramétert átalakítom számmá (megpróbálom) */
+  const userId = parseInt(req.params.userid)
 
+  /* lecsekkolom, hogy sikerült-e átalakítani számmá */
+  if (isNaN(userId)) {
+    console.log(`userid is not a number: ${req.params.userid}`)
+
+    /* ha nem sikerült, küldök egy hibát a frontendnek */
+    res.status(400).json('userId must be a number!!!')
+
+  } else { /* ha sikerült átalakítani, futtatom tovább a kódot */
+    console.log('reading file...')
+
+    /* beolvasom a users.json fájlt */
+    fs.readFile(path.join(__dirname, '/data/users.json'), 'utf8', (err, data) => {
+      /* ha hibát találtam a fájl olvasás közben */
+      if (err) {
+        console.log('error at reading file: ', err)
+
+        res.status(500).send('error at reading file')
+      } else { /* ha nincs hiba, akkor van data */
+        console.log(`reading file was successful, searching for user id: ${userId}`)
+
+        /* a data értéke STRING, átalakítom JS-ben használható típussá (object, array) */
+        const users = JSON.parse(data)
+
+        /* megkeresem az adott userId-t a users adatban */
+        const foundUser = users.find((user) => user.id === userId)
+        
+        if (foundUser) { /* TRUTHY értéket keresek pl. object { id: 2, name: 'John Doe' } */
+          console.log(`found user id: ${userId}, data: ${foundUser}`)
+
+          res.status(200).send(foundUser)
+        } else { /* FALSY értéke van pl. undefined */
+          console.log(`user id: ${userId} was not found`)
+
+          /* ha a find undefined-dal tér vissza, akkor nincs adott ID a users-ben */
+          res.status(404).send(`user id: ${userId} was not found`)
+        }
+      }
+    })
   }
-})
-
-/* elkezdi figyelni az adott portot a számítógépen (localhost vagy 127.0.0.1) */
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
 })
